81bab543e0e5c2053980ae50869e902b387bf06a
Bruno De Masi <brunodemasi1@gmail.com>
2020-06-25 22:30:47 -0300

    feat: scroll movement

---
+++ scripts/scroll.js
@@ -0,0 +1,60 @@
+/* eslint-disable */
+
+const body = document.querySelector('body, html');
+
+const toStartButton = document.querySelector('#goto-start');
+const toConceptButton = document.querySelector('#goto-concept');
+const toObjectiveButton = document.querySelector('#goto-objective');
+const toGroupsButton = document.querySelector('#goto-groups');
+
+const svgStart = document.querySelector('#start-continue'); 
+const svgContinue = document.querySelector('#concept-continue'); 
+const svgObjective = document.querySelector('#objective-continue'); 
+
+const startDiv = document.querySelector('.start');
+const conceptDiv = document.querySelector('.concept-container');
+const objectiveDiv = document.querySelector('.objectives-container');
+const groupsDiv = document.querySelector('.groups-container');
+
+svgStart.addEventListener('click', () => {
+  conceptDiv.scrollIntoView({
+    behavior: 'smooth'
+  })
+});
+
+svgContinue.addEventListener('click', () => {
+  objectiveDiv.scrollIntoView({
+    behavior: 'smooth'
+  })
+});
+
+svgObjective.addEventListener('click', () => {
+  groupsDiv.scrollIntoView({
+    behavior: 'smooth'
+  })
+});
+
+toStartButton.addEventListener('click', () => {
+  startDiv.scrollIntoView({
+    behavior: 'smooth'
+  })
+});
+
+toConceptButton.addEventListener('click', () => {
+  conceptDiv.scrollIntoView({
+    behavior: 'smooth'
+  })
+})
+
+toObjectiveButton.addEventListener('click', () => {
+  console.log('oi')
+  objectiveDiv.scrollIntoView({
+    behavior: 'smooth'
+  })
+})
+
+toGroupsButton.addEventListener('click', () => {
+  groupsDiv.scrollIntoView({
+    behavior: 'smooth'
+  })
+})
